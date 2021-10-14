4c92e3125d07fb4cdb36a0d947b05a0fb19727f7
Albert Gaya <albert.gaya@bluebaytravel.co.uk>
2021-10-14 19:35:24 +0800

    Setup PhpUnit

---
+++ app/Enums/StatusCodeEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+final class StatusCodeEnum
+{
+    const OK = 200;
+    const UNPROCESSABLE_ENTITY = 422;
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,28 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Testing\Assert;
+use Illuminate\Testing\AssertableJsonString;
+use Illuminate\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    private $urlPrefix = '/api';
+
+    protected function uri(string $uri): string
+    {
+        return $this->urlPrefix . $uri;
+    }
+
+    protected function assertSuccessJsonResponse(TestResponse $response): AssertableJsonString
+    {
+        return $response->assertJsonStructure(['data', 'message'])->decodeResponseJson();
+    }
+
+    protected function assertErrorJsonResponse(TestResponse $response): AssertableJsonString
+    {
+        return $response->assertJsonStructure(['errors', 'message'])->decodeResponseJson();
+    }
 }
